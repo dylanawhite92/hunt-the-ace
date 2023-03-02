1a97bbb20ee77ef11d4247336cd8dc3da4fd137f
The Crystal King <dylanawhite11@gmail.com>
2023-03-02 11:54:52 -0600

    declare card part vars

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ const cardObjectDefinitions = [
     {id: 2, imagePath: "/images/card-JackClubs.png"},
     {id: 3, imagePath: "/images/card-QueenDiamonds.png"},
     {id: 4, imagePath: "/images/card-AceSpades.png"},
-]
+];
+
+const createCard = (cardItem) => {
+    // Generate elements that make up cards
+    const cardElem = createElement("div");
+    const cardInnerElem = createElement("div");
+    const cardFrontElem = createElement("div");
+    const cardBackElem = createElement("div");
+
+    // Generate front & back
+    const cardFrontImg = createElement("img");
+    const cardBackImg = createElement("img");
+}
+
+const createElement = (elemType) => {
+    return document.createElement(elemType);
+}
