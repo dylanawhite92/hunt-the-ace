704fcd5be2ca5221d44b99cfb0717a0769518b00
The Crystal King <dylanawhite11@gmail.com>
2023-03-02 12:05:39 -0600

    assigning classes and ids to elements

--- index.js
+++ index.js
@@ -11,12 +11,30 @@ const createCard = (cardItem) => {
     const cardInnerElem = createElement("div");
     const cardFrontElem = createElement("div");
     const cardBackElem = createElement("div");
-
-    // Generate front & back
     const cardFrontImg = createElement("img");
     const cardBackImg = createElement("img");
+
+    // Add ID & class to overall card
+    addClassToElement(cardElem, "card");
+    addIdToElement(cardElem, cardItem.id);
+
+    // Add classes to card elements
+    addClassToElement(cardElem, "card-inner");
+    addClassToElement(cardElem, "card-front");
+    addClassToElement(cardElem, "card-back");
+
 }
 
 const createElement = (elemType) => {
     return document.createElement(elemType);
+}
+
+// Reusable method that adds class to HTML element
+function addClassToElement(elem, className) {
+    elem.classList.add(className);
+}
+
+// Adds ID to HTML element
+function addIdToElement(elem, id) {
+    elem.id = id;
 }
